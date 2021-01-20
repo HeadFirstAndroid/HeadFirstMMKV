5a14655dd844467ed600a5e55919a05b475b69fc
zhening.czn <zhening.czn@alibaba-inc.com>
2021-01-20 10:33:48 +0800

    update

--- app/src/main/java/me/yifeiyuan/hf/mmkv/App.kt
+++ app/src/main/java/me/yifeiyuan/hf/mmkv/App.kt
@@ -1,16 +1,25 @@
 package me.yifeiyuan.hf.mmkv
 
 import android.app.Application
+import me.yifeiyuan.adh.DebugConfig
+import me.yifeiyuan.adh.DebugHelper
 
 /**
  * Created by 程序亦非猿 on 2021/1/19.
  */
 class App : Application() {
 
-
     override fun onCreate() {
         super.onCreate()
-
-
+        DebugConfig().apply {
+            debuggable = true
+            application = this@App
+            enableStrictMode = false
+            detectActivityLifecycle = true
+            detectFragmentLifecycle = true
+            logLevel = DebugConfig.LogLevel.I
+        }.also {
+            DebugHelper.setup(it)
+        }
     }
 }
--- app/src/main/java/me/yifeiyuan/hf/mmkv/MainActivity.kt
+++ app/src/main/java/me/yifeiyuan/hf/mmkv/MainActivity.kt
@@ -1,11 +1,22 @@
 package me.yifeiyuan.hf.mmkv
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import me.yifeiyuan.adh.showcase.AdhShowcaseActivity
+import me.yifeiyuan.adh.showcase.AdhShowcaseItem
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AdhShowcaseActivity() {
+
+    override fun provideShowcaseItems(): List<AdhShowcaseItem> {
+
+        return mutableListOf(
+
+            createShowcaseItem("测试初始化默认") {
+                Toast.makeText(this@MainActivity, "测试初始化默认", Toast.LENGTH_SHORT).show()
+            },
+            createShowcaseItem("测试存入") {
+                Toast.makeText(this@MainActivity, "测试存入", Toast.LENGTH_SHORT).show()
+            }
+        )
     }
 }
